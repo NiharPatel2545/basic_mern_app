dca19ae0185dcc1d809da8db037a3bb41285a521
nihar patel <niharpatel2545@gmail.com>
2025-05-05 14:25:17 +0530

    bug fix second attempt

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const express = require("express"); // Use CommonJS loading
+import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { connectDB } from "./config/db.js";
@@ -17,7 +15,7 @@ app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV.trim() === "production") {
+if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
