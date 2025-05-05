8a0dc758eba20608cef841a3d15d3ec0ed1f71bc
nihar patel <niharpatel2545@gmail.com>
2025-05-05 12:06:51 +0530

    Initial commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,56 +1,57 @@
-import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { Container, SimpleGrid, Text, VStack, useColorModeValue } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useProductStore } from "../store/product";
 import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
-	const { fetchProducts, products } = useProductStore();
+  const { fetchProducts, products } = useProductStore();
+  const gradient = useColorModeValue("linear(to-r, blue.600, cyan.500)", "linear(to-r, purple.600, blue.500)");
 
-	useEffect(() => {
-		fetchProducts();
-	}, [fetchProducts]);
-	console.log("products", products);
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
 
-	return (
-		<Container maxW='container.xl' py={12}>
-			<VStack spacing={8}>
-				<Text
-					fontSize={"30"}
-					fontWeight={"bold"}
-					bgGradient={"linear(to-r, cyan.400, blue.500)"}
-					bgClip={"text"}
-					textAlign={"center"}
-				>
-					Current Products 🚀
-				</Text>
+  return (
+    <Container maxW='container.xl' py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={gradient}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
 
-				<SimpleGrid
-					columns={{
-						base: 1,
-						md: 2,
-						lg: 3,
-					}}
-					spacing={10}
-					w={"full"}
-				>
-					{products.map((product) => (
-						<ProductCard key={product._id} product={product} />
-					))}
-				</SimpleGrid>
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={10}
+          w={"full"}
+        >
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
 
-				{products.length === 0 && (
-					<Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
-						No products found 😢{" "}
-						<Link to={"/create"}>
-							<Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
-								Create a product
-							</Text>
-						</Link>
-					</Text>
-				)}
-			</VStack>
-		</Container>
-	);
+        {products.length === 0 && (
+          <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+            No products exist yet 😢{" "}
+            <Link to={"/create"}>
+              <Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
+                Create a product
+              </Text>
+            </Link>
+          </Text>
+        )}
+      </VStack>
+    </Container>
+  );
 };
+
 export default HomePage;
