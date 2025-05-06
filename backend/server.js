70c739a319f87a529a0d4fed215227998aa829fb
nihar patel <niharpatel2545@gmail.com>
2025-05-06 10:53:24 +0530

    update env code

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ app.use("/api/products", productRoutes);
 if (process.env.NODE_ENV.trim() === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-}
+  }
 
 app.listen(PORT, () => {
 	connectDB();
